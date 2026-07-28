3311b5c55c94b087003daab08fbbfb024c7fbbc3
s <s@LAPTOP-S8BLMRE6>
2026-07-28 19:33:29 +0200

    api-gateway i naming server

---
+++ api-gateway/src/main/java/com/agilne/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,10 @@
+package com.agilne.apigateway;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+}
